ee683dfc2f21b2270cd6e7bc81070e50711db54b
304654716@qq.com <304654716@qq.com@c2ddd387-802b-67f7-7e72-08454b2f5ffd>
2012-03-26 08:47:08 +0000

    头信息

--- trunk/jquery.app.js
+++ trunk/jquery.app.js
@@ -1,4 +1,16 @@
-
+/**
+ * Created by ____′↘夏悸，Universe，風亦飛 
+ * User: ____′↘夏悸
+ * Date: 12-3-26
+ * 这个是一个jQuery Easyui的的桌面扩展
+ * 讨论学习群：
+ *    142872541（一）
+ *    47729185（二）
+ *    70168958（500人超级群）
+ *    腾讯微博群号：83952631
+ *    社区： http://bbs.btboys.com
+ * version: 0.1
+ */
 (function ($) {
     var loaded = false;
     /**
