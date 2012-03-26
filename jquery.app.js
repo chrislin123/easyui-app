05704a16e72c4b8c42299e77605406630572d6b1
304654716 <304654716@qq.com>
2012-03-26 07:26:43 +0000

    初始化

---
+++ jquery.app.js
@@ -0,0 +1,7 @@
+(function ($) {
+	$.fn.app = function (opt, params) {};
+	$.fn.app.methods = {};
+	$.fn.app.parseOptions = function () {};
+	$.fn.app.defaults = {};
+	$.parser.plugins.push('app');
+})(jQuery);
