3724fadd5edf15f9c648d9bbd3161e9123ab45b7
304654716 <304654716@qq.com>
2012-03-26 08:29:14 +0000

    就格式化了一下··

--- jquery.app.js
+++ jquery.app.js
@@ -1,37 +1,30 @@
 (function ($) {
-  var loaded = false;
+	var loaded = false;
 	var init = [{
-		status:'桌面',
-    	handler:function () {
-    		
-    	}
-	},{
-		status:'开始菜单',
-    	handler:function () {
-    		
-    	}
-	},{
-		status:'日历',
-    	handler:function () {
-    		
-    	}
-	},{
-		status:'加载数据',
-    	handler:function () {
-    		
-    	}
-	}];
-   
-  function init(target,params){
-    var opt = $.data(target,'app').options;
-    for (var i in init) {
-				var step = init[i];
-				console.info(step.status);
-				step.handler.call();
-		 }
-		 loaded = true;
-  }
-   
+			status : '桌面',
+			handler : function () {}
+		}, {
+			status : '开始菜单',
+			handler : function () {}
+		}, {
+			status : '日历',
+			handler : function () {}
+		}, {
+			status : '加载数据',
+			handler : function () {}
+		}
+	];
+	
+	function init(target, params) {
+		var opt = $.data(target, 'app').options;
+		for (var i in init) {
+			var step = init[i];
+			console.info(step.status);
+			step.handler.call();
+		}
+		loaded = true;
+	}
+	
 	$.fn.app = function (opt, params) {};
 	$.fn.app.methods = {};
 	$.fn.app.parseOptions = function () {};
