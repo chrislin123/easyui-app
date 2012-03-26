9c2781dceb42b70324204f3f0a5cb4ce5c22289b
304654716@qq.com <304654716@qq.com@c2ddd387-802b-67f7-7e72-08454b2f5ffd>
2012-03-26 15:20:41 +0000

    layout实例化

--- trunk/jquery.app.js
+++ trunk/jquery.app.js
@@ -12,7 +12,7 @@
  * version: 0.1
  */
 (function ($) {
-    var loaded = false;//初始化状态
+    var loaded = false; //初始化状态
 
     /**
      * layout初始化
@@ -23,21 +23,40 @@
         var state = $.data(target, 'app');
         var opts = state.options;
 
-        var center = $('<div/>').attr({'border':false, 'region':'center'}).css({'overflow':'hidden'}).appendTo(jqTarget);
-        if (jqTarget.context.nodeName !== 'BODY')//非body对象，添加fit属性
+        var center = $('<div/>').attr({
+            'border' : false,
+            'region' : 'center'
+        }).css({
+                overflow : 'hidden'
+            }).appendTo(jqTarget);
+        //墙纸设置
+        if (opts.wallpaper) {
+            center.css('background', 'url("' + opts.wallpaper + '") fixed no-repeat center');
+        }
+        if (jqTarget.context.nodeName !== 'BODY') //非body对象，添加fit属性
             jqTarget.attr('fit', true);
 
-        var region = {south:true, north:true, west:true, east:true};
+        var region = {
+            south : true,
+            north : true,
+            west : true,
+            east : true
+        };
+        if (!region[opts.taskBlankPos]) //有效值验证
+            opts.taskBlankPos = 'south';
 
-        if (!region[opts.taskBlankPos])       //有效值验证
-            opts.taskBlankPos = 'north';
-
-        var taskBlank = $('<div/>').attr({'border':false, 'region':opts.taskBlankPos}).css({'overflow':'hidden'}).appendTo(jqTarget);
+        var taskBlank = $('<div/>').attr({
+            'border' : false,
+            'region' : opts.taskBlankPos
+        }).css({
+                overflow : 'hidden'
+            }).appendTo(jqTarget);
         if (opts.taskBlankPos == 'north' || opts.taskBlankPos == 'south') {
             taskBlank.css("height", 35);
         } else {
             taskBlank.css("width", 35);
         }
+        //执行layout实例
         jqTarget.layout();
     }
 
@@ -45,41 +64,31 @@
      * 初始化任务栏
      * @param target
      */
-    function initTaskBlank(target) {
-
-    }
+    function initTaskBlank(target) {}
 
     /**
      *初始化桌面
      * @param target
      */
-    function initDeskTop(target) {
-
-    }
+    function initDeskTop(target) {}
 
     /**
      *初始化开始菜单
      * @param target
      */
-    function initStartMenu(target) {
-
-    }
+    function initStartMenu(target) {}
 
     /**
      *初始化时间
      * @param target
      */
-    function initCalendar(target) {
-
-    }
+    function initCalendar(target) {}
 
     /**
      *初始化widget
      * @param target
      */
-    function initWidget(target) {
-
-    }
+    function initWidget(target) {}
 
     /**
      * 第一步是初始化layout
@@ -89,41 +98,35 @@
      * 第五步初始化时间
      * 第六步初始化widget
      */
-    var init = [
-        {
-            status:$.fn.app.defaults.lang.initLayout,
-            handler:initLayout
-        },
-        {
-            status:$.fn.app.defaults.lang.initTaskBlank,
-            handler:initTaskBlank
-        },
-        {
-            status:$.fn.app.defaults.lang.initDeskTop,
-            handler:initDeskTop
-        },
-        {
-            status:$.fn.app.defaults.lang.initStartMenu,
-            handler:initStartMenu
-        },
-        {
-            status:$.fn.app.defaults.lang.initCalendar,
-            handler:initCalendar
-        },
-        {
-            status:$.fn.app.defaults.lang.initWidget,
-            handler:initWidget
-        }
-    ];
+    var initMethods = [initLayout, initTaskBlank, initDeskTop, initStartMenu, initCalendar, initWidget];
 
+    /**
+     * 初始化
+     * @param target
+     * @param options
+     */
     function init(target, options) {
-        var progress = $('<div/>').appendTo('body');
-        for (var i in init) {
-            var step = init[i];
-            progress.progressbar({width:400, value:((i + 1) / init.length), text:step.status});
-            step.handler.call(this, target);
+        if (loaded)
+            return;
+
+        $('body').attr('oncontextmenu', 'return false'); //禁用全局右键菜单
+
+        var progress = $.messager.progress({ //实例化进度条
+            title : options.lang.progress.title,
+            msg : options.lang.progress.msg,
+            interval : null
+        });
+
+        var progressBar = $.messager.progress('bar'); //获取进度条实例
+
+        for (var i in initMethods) {
+            var step = initMethods[i];
+            progressBar.progressbar({
+                text : options.lang[step.name]
+            }).progressbar('setValue', ((parseInt(i) + 1) / initMethods.length) * 100);
+            step.call(this, target);
         }
-        progress.remove();
+        $.messager.progress('close');
         loaded = true;
     }
 
@@ -140,7 +143,7 @@
             } else {
                 options = $.extend({}, $.fn.app.defaults, $.fn.app.parseOptions, options);
                 $.data(this, 'app', {
-                    options:options
+                    options : options
                 });
             }
 
@@ -150,18 +153,22 @@
 
     $.fn.app.methods = {};
 
-    $.fn.app.parseOptions = function () {
-    };
+    $.fn.app.parseOptions = function () {};
 
     $.fn.app.defaults = {
-        taskBlankPos:'north', //任务栏的位置（north|south|west|east）
-        lang:{
-            initLayout:"init layout",
-            initTaskBlank:"init task blank",
-            initDeskTop:"init desktop",
-            initStartMenu:"init start menu",
-            initCalendar:"init calendar",
-            initWidget:"init widget"
+        taskBlankPos : 'south', //任务栏的位置（north|south|west|east）
+        wallpaper : 'wallpaper.jpg',
+        lang : { //国际化
+            initLayout : "init layout",
+            initTaskBlank : "init task blank",
+            initDeskTop : "init desktop",
+            initStartMenu : "init start menu",
+            initCalendar : "init calendar",
+            initWidget : "init widget",
+            progress : {
+                title : 'Please waiting',
+                msg : 'Loading data...'
+            }
         }
     };
 
